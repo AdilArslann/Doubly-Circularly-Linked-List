8028b85f7294696ee559971664df0ec092f0c4d7
Adil Arslan <adil.arslan@stud.vilniustech.lt>
2022-10-03 21:18:55 +0300

    some more changes

--- program2.cpp
+++ program2.cpp
@@ -8,3 +8,44 @@ struct node
 	struct node* previous;
 };
 
+void insertBeginning(struct Node** start, int value)
+{
+	struct Node* last = (*start)->prevoius;
+
+	struct Node* temp = new Node;
+	temp->data = value;
+
+	temp->next = *start;
+	temp->data = value;
+
+	last->next = (*start)->previous = temp;
+	*start = temp;
+}
+
+void insertMiddle(struct Node** start)
+{
+
+}
+void insertEnd(struct Node** start, int value)
+{
+	if(*start == NULL)
+	{
+		struct Node* temp = new Node;
+		temp->data = value;
+		temp->next = temp->previous = temp;
+		*start = temp;
+		return;
+	}
+
+	Node* last = (*start)->previous;
+
+	struct Node* temp = new Node;
+	temp->data = value;
+
+	temp->next = *start;
+
+	(*start)->previous = temp;
+
+	temp->previous = last;
+	last->next = temp;
+}
